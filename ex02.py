8022c26bfc7a753a24e1fc3cc911cad210fc03f9
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-04-27 12:57:13 -0300

    Add files via upload
    usado print- input

---
+++ ex02.py
@@ -0,0 +1,12 @@
+#Solicitando o usuario digitar um numero
+
+num1 =  int(input("Digite um numero : "))
+num2 = int(input("Digite um numero: "))
+
+
+# realizar a soma
+
+soma =  num1+num2
+
+#exibir o resultado
+print("A soma de", num1, "e" ,num2, "é igual a : ", soma)
