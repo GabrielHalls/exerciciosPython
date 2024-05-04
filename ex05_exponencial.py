fa064214540e22959326c53e4447cda1cb1e75f8
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-05-04 10:10:17 -0300

    correção_nome_dos_arquivos
    feita correção  do nome dos arquivos up e adicionado  o exponencial.

---
+++ ex05_exponencial.py
@@ -0,0 +1,10 @@
+print("Vamos realizar um calculo exponencial")
+print("\n")
+
+base = int(input("Digite um numero base: "))
+expo = int (input("Digite o numero expoente: "))
+
+resultado = pow(base,expo)
+
+print("O resultado do valor digitado é : ", resultado)
+
