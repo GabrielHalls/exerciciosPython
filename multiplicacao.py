8dd1882cb090c4f65b44631a6b977a97ef920397
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-05-04 10:11:58 -0300

    Delete multiplicacao.py

--- multiplicacao.py
+++
@@ -1,9 +0,0 @@
-print("Olá seja bem-vindo vamos realizar uma multiplicação  nesse exemplo.")
-print("\n")
-
-num1 = int(input("Digite um numero"))
-num2 = int(input("Digite um numero"))
-
-mult = num1*num2
-
-print("O resultado final  dos valores digitado", mult)
