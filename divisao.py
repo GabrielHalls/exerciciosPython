a71ee9b0b75dcd6fd65284bfb192a073d5289d87
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-05-04 09:10:39 -0300

    Add files via upload
    cálculos matemático básico com as operações de : soma, subtracção, multiplicação e divisão.

---
+++ divisao.py
@@ -0,0 +1,9 @@
+print("Olá seja bem-vindo vamos realizar uma divisão  nesse exemplo.")
+print("\n")
+
+num1 = float(input("Digite um numero: "))
+num2 = float(input("Digite um numero: "))
+
+div = num1 / num2
+
+print("O resultado dos valores digitado é: ", div)
