88efc74f13d1fdacb8eb8d2c716c825b28c6959d
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-05-04 10:11:41 -0300

    Delete subtracao.py

--- subtracao.py
+++
@@ -1,9 +0,0 @@
-print("Olá seja bem-vindo vamos realizar uma subtração nesse exemplo.")
-print("\n")
-
-num1 = int (input("Digite um numero: "))
-num2 = int (input("Digite um numero: "))
-
-sub =  num1-num2
-
-print("O resultado dos valores digitado: ", sub)
