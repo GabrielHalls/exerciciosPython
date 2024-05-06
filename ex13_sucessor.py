6683401352dd9f0ec81c25b8be9b4fad26ce2c2b
Gabriel Vinicius de Deus Sousa <111321165+GabrielHalls@users.noreply.github.com>
2024-05-06 15:24:42 -0300

    add _exercicios_13e14

---
+++ ex13_sucessor.py
@@ -0,0 +1,12 @@
+# Curso básico de Python
+# Nome do desenvolvedor: Gabriel Vinicius
+# Versão 1.0
+# Exercicicos de logica de programação
+# com logica de programação em python
+# Exercicios de encontrar o numero sucessor
+
+print("Decrementação numerica \n")
+
+num1 = int (input("Digite um numero: "))
+resultado =  num1 ++1
+print("O antecessor do numero digitado é : ", resultado)
---
+++ ex14_trapezio.py
@@ -0,0 +1,16 @@
+# Curso básico de Python
+# Nome do desenvolvedor: Gabriel Vinicius
+# Versão 1.0
+# Exercicicos de logica de programação
+# com logica de programação em python
+# Exercicios de encontrar a area de um trapézio
+
+print("Vamos encontrar a área de um trapézio ? \n")
+
+baseMaior = float(input("Digite o valor da (Base Maior):  "))
+baseMenor = float(input("Digite o valor da (Base Menor):  "))
+altura = float(input("Digite o valor da altura do trapézio:  "))
+
+resultado = (baseMaior+baseMenor)*altura/2
+
+print("O resultado final com base nos valores fornecidos é : ", resultado)
